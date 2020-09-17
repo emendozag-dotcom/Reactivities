790bffd540973c0b49f8b756928f315dadd825f9
Eliel Mendoza <developer.emg@gmail.com>
2020-09-17 01:41:40 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,7 +26,14 @@ namespace API {
             services.AddDbContext<DataContext> (opt => {
                 opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
-            
+            services.AddCors (opt => {
+                opt.AddPolicy ("CorsPolicy", policy => {
+                    policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
+
+                });
+
+            });
+
             services.AddControllers ();
         }
 
@@ -41,6 +48,7 @@ namespace API {
             app.UseRouting ();
 
             app.UseAuthorization ();
+            app.UseCors ("CorsPolicy");
 
             app.UseEndpoints (endpoints => {
                 endpoints.MapControllers ();
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import { Button, Header, Icon, List } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IAppProps {}
+interface IAppState {
+  values: Array<{ id: number; name: string }>;
+  loading: boolean;
+  disabled: boolean;
+}
+
+class App extends Component {
+  state: IAppState = {
+    values: [],
+    loading: false,
+    disabled: false,
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+      });
+    });
+  }
+
+  handleRefresh = () => {
+    this.setState({
+      loading: !this.state.loading,
+      disabled: !this.state.disabled,
+    });
+
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+        loading: false,
+        disabled: false,
+      });
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header as="h2">
+          <Icon name="user"/>
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+            {this.state.values.map((value: any) => (
+              <List.Item key={value.id}>{value.name}</List.Item>
+            ))}
+          </List>
+      </div>
+    );
+  }
 }
 
 export default App;
